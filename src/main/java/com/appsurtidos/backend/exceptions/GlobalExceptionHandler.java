e31cb924a317f58651692375af50ecbbe62a17f5
Joaquin-Developer <joaquin.p.olivera@gmail.com>
2025-03-23 21:02:54 -0300

    APPS-31 - swagger

--- src/main/java/com/appsurtidos/backend/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/appsurtidos/backend/exceptions/GlobalExceptionHandler.java
@@ -4,14 +4,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@RestControllerAdvice
+/*
+@ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(MissingServletRequestParameterException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<Map<String, String>> handleMissingParam(MissingServletRequestParameterException ex) {
         Map<String, String> response = new HashMap<>();
         response.put("error", "Param not found: " + ex.getParameterName());
@@ -19,6 +22,7 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseEntity<Map<String, String>> handleGeneralException(Exception ex) {
         Map<String, String> response = new HashMap<>();
         response.put("error", "Internal Server Error");
@@ -28,3 +32,4 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
     }
 }
+*/
